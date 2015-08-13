0c3c780ca3a9642c1fe761ec84951a2388d675d1
urveshtanna <ubtanna@gmail.com>
2015-08-13 22:13:12 +0530

    Voice recognisation intregrated.

--- app/src/main/java/urvesh/com/somaiya/VoiceMain.java
+++ app/src/main/java/urvesh/com/somaiya/VoiceMain.java
@@ -65,7 +65,7 @@ public class VoiceMain extends AppCompatActivity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Voice recognition Demo...");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "");
         startActivityForResult(intent, REQUEST_CODE);
     }
 
